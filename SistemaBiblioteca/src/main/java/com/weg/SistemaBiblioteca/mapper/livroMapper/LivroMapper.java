88b1f607f1acddba5eec38f681bc9af747376dac
Viihh-77 <gabrielli_glowatski@estudante.sesisenai.org.br>
2026-02-24 18:13:04 -0300

    Adicionando classe mapper ao livro

--- SistemaBiblioteca/src/main/java/com/weg/SistemaBiblioteca/mapper/livroMapper/LivroMapper.java
+++ SistemaBiblioteca/src/main/java/com/weg/SistemaBiblioteca/mapper/livroMapper/LivroMapper.java
@@ -3,7 +3,12 @@ package com.weg.SistemaBiblioteca.mapper.livroMapper;
 import com.weg.SistemaBiblioteca.dto.livroDto.LivroRequisicaoDto;
 import com.weg.SistemaBiblioteca.dto.livroDto.LivroRespostaDto;
 import com.weg.SistemaBiblioteca.model.Livro;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class LivroMapper {
 
     public Livro paraEntidade(LivroRequisicaoDto livroRequisicaoDto) {
@@ -21,5 +26,14 @@ public class LivroMapper {
                 livro.getAutor(),
                 livro.getAnoPublicacao()
         );
+    }
+
+    public List<LivroRespostaDto> paraListaResposta(List<Livro> livros) {
+        List<LivroRespostaDto> listaResposta = new ArrayList<>();
+
+        for (Livro livro : livros) {
+            listaResposta.add(paraRespostaDto(livro));
+        }
+        return listaResposta;
     }
 }
